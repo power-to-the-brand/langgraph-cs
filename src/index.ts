f321a89e146d12d1a11c44f394a16c5f37d3580e
Tek Loon <tl@powertothebrand.sg>
2024-06-18 13:15:38 +0800

    feat: add MemorySaver for Persistence

--- src/index.ts
+++ src/index.ts
@@ -3,38 +3,39 @@ dotenv.config();
 
 import readline from "readline";
 import { ChatGroq } from "@langchain/groq";
-import { END, START, StateGraph, StateGraphArgs } from "@langchain/langgraph";
+import {
+  END,
+  MemorySaver,
+  START,
+  StateGraph,
+  StateGraphArgs,
+} from "@langchain/langgraph";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+import { BaseMessage } from "@langchain/core/messages";
 
 const chatGroq = new ChatGroq({
   apiKey: process.env.GROQ_API_KEY,
   model: "llama3-8b-8192",
 });
 
+async function gradeConversation(state: GraphState) {}
+
 async function generate(state: GraphState) {
-  const question = state.question;
+  const { messages } = state;
 
-  const template = `Answer the below question. \n
-    \n
-    Question: \n
-    {question} \n
-    `;
-
-  const prompt = PromptTemplate.fromTemplate(template);
-
-  const chain = prompt.pipe(chatGroq).pipe(new StringOutputParser());
-
-  const result = await chain.invoke({ question });
+  const result = await chatGroq.invoke(messages);
 
   return {
-    generation: result,
+    generation: result.content.toString(),
+    messages: [result],
   };
 }
 
 type GraphState = {
   question: string;
   generation?: string;
+  messages: BaseMessage[];
 };
 
 const graphState: StateGraphArgs<GraphState>["channels"] = {
@@ -45,6 +46,10 @@ const graphState: StateGraphArgs<GraphState>["channels"] = {
   generation: {
     value: (left?: string, right?: string) => (right ? right : left),
     default: () => undefined,
+  },
+  messages: {
+    value: (x: BaseMessage[], y: BaseMessage[]) => x.concat(y),
+    default: () => [],
   },
 };
 
@@ -57,7 +62,8 @@ const workflow = new StateGraph<GraphState>({ channels: graphState }).addNode(
 workflow.addEdge(START, "generate");
 workflow.addEdge("generate", END);
 
-const app = workflow.compile();
+const memory = new MemorySaver();
+const app = workflow.compile({ checkpointer: memory });
 
 const threadId = "conversation-1";
 
@@ -74,21 +80,24 @@ const promptQuestion = async () => {
 };
 
 const execute = async (question: string) => {
-  const inputs = { question };
-  const config = { recursionLimit: 50, threadId };
-  let finalGeneration;
-  for await (const output of await app.stream(inputs, config)) {
-    for (const [key, value] of Object.entries(output)) {
-      console.log(`Node: '${key}'`);
-      // Optional: log full state at each node
-      // console.log(JSON.stringify(value, null, 2));
-      finalGeneration = value;
-    }
-    console.log("\n---\n");
-  }
+  const messages = [["user", question]];
+  const inputs = { messages };
 
-  // Log the final generation.
-  console.log("Sytem: " + JSON.stringify(finalGeneration, null, 2));
+  const config = { recursionLimit: 50, configurable: { threadId } };
+  for await (const { messages } of await app.stream(inputs, {
+    ...config,
+    streamMode: "values",
+  })) {
+    let msg = messages[messages?.length - 1];
+    if (msg?.content) {
+      console.log("System: " + msg.content);
+    } else if (msg?.tool_calls?.length > 0) {
+      console.log(msg.tool_calls);
+    } else {
+      console.log(msg);
+    }
+    console.log("-----\n");
+  }
 };
 
 promptQuestion();
